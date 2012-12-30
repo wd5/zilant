51ffe0367961b570ec312ce7ecde47dd9ef8adb1
Mikhail Polykovskij <glader@yandex-team.ru>
2012-12-31 00:08:16 +0600

    Static

--- src/articles/templatetags/articles.py
+++ src/articles/templatetags/articles.py
@@ -12,3 +12,13 @@ def content(article_id):
         return Article.objects.get(pk=article_id).content
     else:
         return Article.objects.get(slug=article_id).content
+
+
+@register.inclusion_tag("articles/article_list.html")
+def children(article_id):
+    if article_id.isdigit():
+        parent = Article.objects.get(pk=article_id)
+    else:
+        parent = Article.objects.get(slug=article_id)
+
+    return {'articles': Article.objects.filter(parent=parent)}
